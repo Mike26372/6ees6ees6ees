cdade834f8b0dacf3c6ecfa01a56463bc80a723a
Michael Tutt <tutt.michael@gmail.com>
2016-12-19 11:09:20 -0800

    Add Bee class

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,12 @@
-class Bee {
-  // TODO..
-};
+class Bee extends Grub {
+  constructor (age, color, job) {
+    super();
+    this.age = 5;
+    this.color = 'yellow';
+    this.job = 'Keep on growing';
+  }
+}
+
+// Bee.prototype = Object.create(Grub.prototype);
+// Bee.prototype.constructor = Bee;
+
